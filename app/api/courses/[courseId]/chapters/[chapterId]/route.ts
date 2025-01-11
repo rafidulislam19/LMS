d3a4af3e3438349f860ce2fc44764a65ca8d535e
rafidulislam19 <you@example.com>
2025-01-12 00:42:55 +0600

    course table,search

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,103 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import Mux from "@mux/mux-node";
 
 import { db } from "@/lib/db";
+
+// const { Video } = new Mux(
+//     process.env.MUX_TOKEN_ID!, 
+//     process.env.MUX_TOKEN_SECRET!,
+//   );
+
+const mux = new Mux({
+    tokenId: process.env.MUX_TOKEN_ID,
+    tokenSecret: process.env.MUX_TOKEN_SECRET
+  });
+
+// Access the Video API from the Mux instance
+const { video } = mux;
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { courseId: string; chapterId: string } }
+) {
+    try {
+        const { userId } = await auth();
+
+        if(!userId ) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const ownCourse = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId,
+            }
+        });
+
+        if (!ownCourse) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const chapter = await db.chapter.findUnique({
+            where: {
+                id: params.chapterId,
+                courseId: params.courseId,
+            },
+        });
+
+        if (!chapter) {
+            return new NextResponse("Chapter not found", { status: 404 });
+        }
+
+        if(chapter.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId,
+                }
+            });
+
+            if(existingMuxData) {
+                await video.assets.delete(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id,
+                    }
+                });
+            }
+        }
+
+        const deletedChapter = await db.chapter.delete({
+            where: {
+                id: params.chapterId
+            }
+        });
+
+        const publishedChapterInCourse = await db.chapter.findMany({
+            where: {
+                courseId: params.courseId,
+                isPublished: true,
+            }
+        })
+
+        if(!publishedChapterInCourse.length) {
+            await db.course.update({
+                where: {
+                    id: params.courseId,
+                },
+                data: {
+                    isPublished: false,
+                }
+            });
+        }
+
+        return NextResponse.json(deletedChapter);
+
+    } catch (error) {
+        console.log("[CHAPTER_ID_DELETE]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
 
 export async function PATCH(
     req: Request,
@@ -36,11 +132,39 @@ export async function PATCH(
             }
         });
 
+        if(values.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId,
+                }
+            });
+
+            if(existingMuxData) {
+                await video.assets.delete(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id,
+                    }
+                });
+            }
+
+            const asset = await mux.video.assets.create({
+                input: values.videoUrl,
+                playback_policy: ['public'],
+                test: false,
+                video_quality: 'basic',
+            });
+
+            await db.muxData.create({
+                data: {
+                    chapterId: params.chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id,
+                }
+            });
+        }
+
         return NextResponse.json(chapter);
-
-        // TODO: Handle video upload
-
-
     } catch (error) {
         console.log("[COURSES_CHAPTER_ID]", error);
         return new NextResponse("Internal Error", { status: 500 });
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,14 +5,22 @@ import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.includes("/chapter");
+    const isSearchPage = pathname === "/search";
 
     return ( 
+        <>
+        { isSearchPage && (
+            <div className="hidden md:block">
+                <SearchInput />
+            </div>
+        )}
         <div className="flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
                 <Link href="/">
@@ -31,5 +39,6 @@ export const NavbarRoutes = () => {
             <UserButton
             afterSignOutUrl="/sign-in" />
         </div>
+        </>
      );
 }
