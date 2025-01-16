e3143d0af31c9984dfd74bc18fa1c141eb3fd37f
rafidulislam19 <you@example.com>
2025-01-16 22:39:50 +0600

    course progress, payment

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { Loader2, Lock } from "lucide-react";
 import { cn } from "@/lib/utils";
 import MuxPlayer from "@mux/mux-player-react";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
 
 interface VideoPlayerProps {
     playbackId: string;
@@ -28,6 +29,31 @@ export const VideoPlayer = ({
     title,
 }: VideoPlayerProps) => {
     const [isReady, setIsReady] = useState(false);
+    const router = useRouter();
+    const confetti = useConfettiStore();
+
+    const onEnd = async () => {
+        try {
+            if (completeOnEnd) {
+                await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, {
+                    isCompleted: true,
+                });
+
+                if (!nextChapterId) {
+                    confetti.onOpen();
+                }
+
+                toast.success("Progress updated!");
+                router.refresh();
+
+                if(nextChapterId) {
+                    router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
+                }
+            }
+        } catch {
+            toast.error("Something went wrong");
+        } 
+    }
 
     return ( 
         <div className="relative aspect-video">
@@ -51,7 +77,7 @@ export const VideoPlayer = ({
                     !isReady && "hidden"
                   )}
                   onCanPlay={() => setIsReady(true)}
-                  onEnded={() => {}}
+                  onEnded={onEnd}
                   onError={(e) => console.error("MuxPlayer Error:", e)}
                   autoPlay
                   playbackId={playbackId}
--- components/course-card.tsx
+++ components/course-card.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { IconBadge } from "@/components/icon-badge";
 import { BookOpen } from "lucide-react";
 import { formatPrice } from "@/lib/format";
+import { CourseProgress } from "@/components/course-progress";
 
 interface CourseCardProps {
     id: string;
@@ -49,9 +50,11 @@ export const CourseCard = ({
                         </div>
                     </div>
                     {progress !== null ? (
-                        <div>
-                            TODO: Progress Component
-                        </div>
+                        <CourseProgress
+                        variant={progress === 100 ? "success" : "default"}
+                            size="sm"
+                            value={progress}
+                        />
                     ): (
                         <p className="text-md md:text-sm font-medium text-slate-700">
                             {formatPrice(price)}
